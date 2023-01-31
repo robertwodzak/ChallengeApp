b9b69ab52f23d4e9d0c0f38d5d046050ead0acba
Robert Wodzak <robertwodzak@gmail.com>
2023-01-31 22:35:35 +0100

    Aplikacja na powitanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Cześć, to ja Robert. Pozdrawiam Was serdecznie z Trójmiasta.");
+Console.WriteLine("Cześć, to ja Robert. Pozdrawiam Was serdecznie z Trójmiasta");
