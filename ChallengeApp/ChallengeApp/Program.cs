2f6e69301ea40fee62779e68c00f060596b7c36c
Robert Wodzak <robertwodzak@gmail.com>
2023-01-31 23:45:54 +0100

    ZadDom04 - Zmienne i podstawowe operacje

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Cześć, to ja Robert. Pozdrawiam Was serdecznie z Trójmiasta");
+// Console.WriteLine("Cześć, to ja Robert. Pozdrawiam Was serdecznie z Trójmiasta");
+string name = "Ewa";
+char gender = 'f'; // niby mogło by być bool, ale w dzisiejszych czasach stosuje się m/f/d 
+int age = 33;
+
+if (gender == 'f' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == 'm' && age < 18)
+{
+    Console.WriteLine("Nieletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Jakaś inna Istota");
+}
