d2206fe2e808dea549968fade3ae619e2e77b522
Robert Wodzak <robertwodzak@gmail.com>
2023-01-31 22:25:15 +0100

    Pierwsza aplikacja

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Cześć, to ja Robert. Pozdrawiam Was serdecznie z Trójmiasta");
