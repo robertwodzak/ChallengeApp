f99c17f7677c9ffd57069d653dadad0b2e905e27
Robert Wodzak <robertwodzak@gmail.com>
2023-02-07 00:28:39 +0100

    ZadDom06 - Klasy i metody w klasie

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,44 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> points = new List<int>();
+        public Employee(string name, string surname, int age, int points) // tu jest konstruktor
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; } // tu są pola
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Sum
+        {
+            get
+            {
+                return this.points.Sum();
+            }
+        }
+        public string emplData
+        {
+            get
+            {
+                return string.Join(" ", this.Name, this.Surname) + " (" + this.Age + ")";
+            }
+        }
+        public void AddScore(int number)  // tu są metody
+        {
+            this.points.Add(number);
+        }
+        public void AddRandomNotes(int howManyNotes)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < howManyNotes; i++)
+            {
+                int points = rand.Next(1, 10);
+                this.AddScore(points);
+            }
+        }
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,30 @@
-﻿int number = 7586267;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-Console.WriteLine("Wyniki dla liczby: "+number);
-
-// ----- stwórz listę counter zawierającą 10 elementów z ilością powtórzeń kolejnych cyfr
-List<int> counter = new List<int>(new int[10]);
-
-// ----- sprawdź każdy element tablicy letters (char zamień na liczbę)
-// ----- dodaj +1 do odpowiedniego indeksu na liście counter
-for (int dig = 0; dig < letters.Length; dig++)
+﻿using ChallengeApp;
+Employee employee1 = new Employee("Mieczysław", "Karasiewicz", 24, 0);
+Employee employee2 = new Employee("Zenobia", "Miłoszyńska", 56, 0);
+Employee employee3 = new Employee("Florian", "Majewski", 33, 0);
+employee1.AddRandomNotes(5); // ----- przydziel po pięć losowych ocen 1-10 pracownikom
+employee2.AddRandomNotes(5);
+employee3.AddRandomNotes(5);
+var result1 = employee1.Sum; // ----- pokaż sumę punktów uzyskanych przez pracowników
+var result2 = employee2.Sum;
+var result3 = employee3.Sum;
+var emplData1 = employee1.emplData; // ----- scal dane osobowe pracowników
+var emplData2 = employee2.emplData;
+var emplData3 = employee3.emplData;
+// ----- stwórz listę z danymi osobowymi pracowników i listę z ich wynikami
+List<string> employees = new List<string> {emplData1, emplData2, emplData3};
+List<int> wyniki = new List<int> {result1, result2, result3};
+// ----- Ile wynosi maks. ilość zdobytych punktów? Kto zdobył? (Co jeśli kilka osób? → lista)
+int maxPoints = wyniki.Max();
+List<string> winner = new List<string>();
+foreach (var res in wyniki.Select((value, i) => new { i, value }))
 {
-    var whichDig = int.Parse(letters[dig].ToString());
-    counter[whichDig]++;
+    var value = res.value;
+    var index = res.i;
+    Console.WriteLine(value + " ◄── " + employees[index]);
+    if (value == maxPoints)
+    {
+        winner.Add(employees[index]);
+    }
 }
-
-// ----- przedstaw rozwiązanie zadania, pokazując po kolei elementy listy counter
-var j = 0;
-foreach (var elem in counter)
-{
-    Console.WriteLine(j + " => " + elem);
-    j++;
-}
+Console.WriteLine("═════════\nZwycięzca to: " + String.Join(", ", winner) + ". \nZdobyte punkty: " + maxPoints+". \nGratulujemy!");
