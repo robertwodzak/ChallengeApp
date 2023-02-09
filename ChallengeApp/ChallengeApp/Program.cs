f6ba26d208755c342414519520c3d0f34c8ca601
Robert Wodzak <robertwodzak@gmail.com>
2023-02-09 01:27:52 +0100

    ZadDom06.a ver. 2 - Klasy i metody w klasie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,30 +1,44 @@
 ﻿using ChallengeApp;
-Employee employee1 = new Employee("Mieczysław", "Karasiewicz", 24, 0);
-Employee employee2 = new Employee("Zenobia", "Miłoszyńska", 56, 0);
-Employee employee3 = new Employee("Florian", "Majewski", 33, 0);
-employee1.AddRandomNotes(5); // ----- przydziel po pięć losowych ocen 1-10 pracownikom
-employee2.AddRandomNotes(5);
-employee3.AddRandomNotes(5);
-var result1 = employee1.Sum; // ----- pokaż sumę punktów uzyskanych przez pracowników
-var result2 = employee2.Sum;
-var result3 = employee3.Sum;
-var emplData1 = employee1.emplData; // ----- scal dane osobowe pracowników
-var emplData2 = employee2.emplData;
-var emplData3 = employee3.emplData;
-// ----- stwórz listę z danymi osobowymi pracowników i listę z ich wynikami
-List<string> employees = new List<string> {emplData1, emplData2, emplData3};
-List<int> wyniki = new List<int> {result1, result2, result3};
-// ----- Ile wynosi maks. ilość zdobytych punktów? Kto zdobył? (Co jeśli kilka osób? → lista)
-int maxPoints = wyniki.Max();
-List<string> winner = new List<string>();
-foreach (var res in wyniki.Select((value, i) => new { i, value }))
+List<Employee> emplObj = new List<Employee>();  // ----- stwórz nową listę z pracownikami jako obiektami
+emplObj.Add(new Employee("Mieczysław", "Karasiewicz", 24, 0));                                      
+emplObj.Add(new Employee("Zenobia", "Miłoszyńska", 56, 0));
+emplObj.Add(new Employee("Florian", "Majewski", 33, 0));
+List<string> winner = new List<string>();       // ----- stwórz pustą listę zwycięzców i ustal maks. wynik jako -1
+var maxScore = -1;
+foreach (var employee in emplObj)       // ----- Dla każdego z pracowników:
+{
+    employee.AddRandomNotes(5);         // ----- 1) przydziel po pięć losowych ocen w skali 1-10
+    var emplData = employee.emplData;   // ----- 2) scal dane osobowe
+    var score = employee.Sum;           // ----- 3) sumuj punkty
+
+    if (score > maxScore)
+    {
+        maxScore = score;
+        winner.Clear();
+        winner.Add(emplData);
+    }
+    else if (score == maxScore)
+    {
+        winner.Add(emplData);
+    }
+}
+Console.WriteLine("Zwycięzca to: " + String.Join(", ", winner) + ". \nZdobyte punkty: " + maxScore + ". \nGratulujemy!");
+
+
+// ----- PONIŻSZY FRAGMENT KODU SŁUŻY TYLKO POKAZANIU, jaką sumę punktów zdobył każdy pracownik i można go usunąć
+Console.WriteLine("═════════");
+List<string> allEmployees = new List<string>();
+List<int> allScores = new List<int>();
+foreach (var employee in emplObj)
+{
+    var emplData = employee.emplData;   // ----- scal dane osobowe
+    var score = employee.Sum;           // ----- sumuj punkty
+    allEmployees.Add(emplData);
+    allScores.Add(score);
+}
+foreach (var res in allScores.Select((value, i) => new { i, value }))
 {
     var value = res.value;
     var index = res.i;
-    Console.WriteLine(value + " ◄── " + employees[index]);
-    if (value == maxPoints)
-    {
-        winner.Add(employees[index]);
-    }
+    Console.WriteLine(value + " ◄── " + allEmployees[index]);
 }
-Console.WriteLine("═════════\nZwycięzca to: " + String.Join(", ", winner) + ". \nZdobyte punkty: " + maxPoints+". \nGratulujemy!");
