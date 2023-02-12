f22854d74b535e0962788afca82f30203f632c54
Robert Wodzak <robertwodzak@gmail.com>
2023-02-12 14:04:07 +0100

    ZadDom07 - Testy jednostkowe

--- ChallengeApp/ChallengeApp.Tests/UserTests.cs
+++ ChallengeApp/ChallengeApp.Tests/UserTests.cs
@@ -3,28 +3,27 @@ namespace ChallengeApp.Tests
     public class Tests
     {
         [Test]
-        public void WhenUserCollectsThreeScores_ShouldReturnCorrectResult()
+        public void WhenUserCollectsThreeScores_ShouldReturnCorrectResult() // TEST #1 - tylko dodawane punkty
         {
             // arrange
             List<User> userObj = new List<User>();  // ----- stwórz now¹ listê z userami jako obiektami
-            userObj.Add(new User("Mieczys³aw", "Karasiewicz", 24, 0));
+            userObj.Add(new User("Jadwiga", "W¹sik", 24, 0));
             userObj[0].AddScore(5);
             userObj[0].AddScore(6);
             userObj[0].AddScore(2);
-            
+
             // act
             var result = userObj[0].Result;
 
             // assert
-            Assert.AreEqual(13, result); 
-        
+            Assert.AreEqual(13, result);
         }
         [Test]
-        public void WhenUserHasPenalty_ShouldSubtractPenaltyFromScore()
+        public void WhenUserHasPenalty_ShouldSubtractPenaltyFromScore() // TEST #2 - dodawane punkty i odejmowane punkty karne
         {
             // arrange
             List<User> userObj = new List<User>();  // ----- stwórz now¹ listê z userami jako obiektami
-            userObj.Add(new User("Mieczys³aw", "Karasiewicz", 24, 0));
+            userObj.Add(new User("Iga", "Michalewska", 19, 0));
             userObj[0].AddScore(2);
             userObj[0].AddScore(4);
             userObj[0].AddScore(7);
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -5,11 +5,10 @@ userObj.Add(new User("Zenobia", "Miłoszyńska", 56, 0));
 userObj.Add(new User("Florian", "Majewski", 33, 0));
 List<string> winner = new List<string>();       // ----- stwórz pustą listę zwycięzców i ustal maks. wynik jako -1
 var maxScore = -1;
-
 // ----- Zdobyte punkty przez userów
 userObj[0].AddScore(7);
 userObj[0].AddScore(9);
-userObj[0].AddScore(5);
+userObj[0].AddScore(10);
 userObj[0].AddScore(8);
 userObj[0].AddScore(10);
 
@@ -24,12 +23,10 @@ userObj[2].AddScore(4);
 userObj[2].AddScore(3);
 userObj[2].AddScore(4);
 userObj[2].AddScore(4);
-
 // ----- Otrzymane punkty ujemne
 userObj[0].SubtractPenalty(38);
-userObj[1].SubtractPenalty(12);
-userObj[2].SubtractPenalty(2);
-
+userObj[1].SubtractPenalty(1);
+userObj[2].SubtractPenalty(5);
 foreach (var user in userObj)       // ----- Dla każdego z userów:
 {
     var userData = user.userData;   // ----- 1) scal dane osobowe
@@ -47,24 +44,3 @@ foreach (var user in userObj)       // ----- Dla każdego z userów:
     }
 }
 Console.WriteLine("Zwycięzca to: " + String.Join(", ", winner) + ". \nZdobyte punkty: " + maxScore + ". \nGratulujemy!");
-
-/*
-// ----- PONIŻSZY FRAGMENT KODU SŁUŻY TYLKO POKAZANIU, jaką sumę punktów zdobył każdy user
-// ----- (można usunąć poniższy fragment kodu)
-Console.WriteLine("═════════");
-List<string> allUsers = new List<string>();
-List<int> allScores = new List<int>();
-foreach (var user in userObj)
-{
-    var userData = user.userData;   // ----- scal dane osobowe
-    var score = user.Result;           // ----- sumuj punkty
-    allUsers.Add(userData);
-    allScores.Add(score);
-}
-foreach (var res in allScores.Select((value, i) => new { i, value }))
-{
-    var value = res.value;
-    var index = res.i;
-    Console.WriteLine(value + " ◄── " + allUsers[index]);
-}
-*/
